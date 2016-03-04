6e8518475993f0bcadb7490a9636e3754b3efd11
leonidas <leonidas@softways.gr>
2016-03-04 12:29:34 +0200

    push notifications dev level

--- www/js/app.js
+++ www/js/app.js
@@ -4,17 +4,18 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ksSwiper', 'ngCordova', 'jett.ionic.filter.bar'])
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'ksSwiper', 'ngCordova', 'jett.ionic.filter.bar'])
 .config(function( $ionicConfigProvider) {
        $ionicConfigProvider.navBar.alignTitle('center');
 })
 
 .constant('appConfig', {
-  appName: 'INOXDOBROS',
-  apiUrl: 'www2.sw.inoxdobros.gr'
+  appName: 'QUALITO',
+  apiUrl: 'http://qualito.softways.gr/api/v1'
 })
 
 .run(function ($ionicPlatform) {
+  
   $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -27,6 +28,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ksSwiper', 'ngCordov
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+    });
   });
 })
 
