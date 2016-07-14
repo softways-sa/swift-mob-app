5bfb2f5ed4c9d180740b4a056e0234cdb67abf0b
Panos Konstantinidis <konstpan@gmail.com>
2016-07-14 14:38:11 +0300

    Changed REST API to shopdemo

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,9 +2,9 @@ angular
   .module('starter')
   
   .constant('appConfig', {
-    appName: 'Softways',
-    serverHost: 'http://www.yourdomain.gr',
-    apiUrl: 'http://www.yourdomain.gr/api/v1',
+    appName: 'Softways Demo',
+    serverHost: 'http://shopdemo.softways.gr',
+    apiUrl: 'http://shopdemo.softways.gr/api/v1',
     googleProjectId: "GOOGLE_PROJECT_ID",
     pushwooshAppId: "PUSHWOOSH_APP_ID"
   });
