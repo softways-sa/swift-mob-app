a3415c515eaab4995b07717dbb3c0098a8165078
Panos Konstantinidis <konstpan@gmail.com>
2016-07-14 17:39:05 +0300

    Modified ids for push notification

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,9 +2,9 @@ angular
   .module('starter')
   
   .constant('appConfig', {
-    appName: 'Softways Demo',
+    appName: 'Softways Mob App Demo',
     serverHost: 'http://shopdemo.softways.gr',
     apiUrl: 'http://shopdemo.softways.gr/api/v1',
-    googleProjectId: "GOOGLE_PROJECT_ID",
-    pushwooshAppId: "PUSHWOOSH_APP_ID"
+    googleProjectId: "853785403702",
+    pushwooshAppId: "78DF5-4C55A"
   });
