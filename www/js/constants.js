e9965baf6bbc703876393bb33cdffb02e255f92e
Panos Konstantinidis <konstpan@gmail.com>
2016-12-12 16:16:11 +0200

    Fixed push notification bug

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,10 +1,11 @@
 angular
-  .module('starter')
-  
-  .constant('appConfig', {
-    appName: 'Softways Mob App Demo',
-    serverHost: 'http://shopdemo.softways.gr',
-    apiUrl: 'http://shopdemo.softways.gr/api/v1',
-    googleProjectId: "853785403702",
-    pushwooshAppId: "78DF5-4C55A"
-  });
+
+.module('starter')
+
+.constant('appConfig', {
+  appName: 'Softways Mob App Demo',
+  serverHost: 'http://shopdemo.softways.gr',
+  apiUrl: 'http://shopdemo.softways.gr/api/v1',
+  googleProjectNumber: "853785403702",
+  pushwooshAppId: "78DF5-4C55A"
+});
